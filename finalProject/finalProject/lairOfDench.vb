3e1c3dc4c137e7fd5a89217836fc668c2a6287e3
DGGKRyan <ryanc18dg@gmail.com>
2017-12-14 11:30:02 -0600

    yommers

---
+++ finalProject/finalProject/lairOfDench.vb
@@ -0,0 +1,17 @@
+﻿Public Class lairOfDench
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles timeLeft.Tick
+
+        Dim timeLeft As New Integer
+        If timeLeft > 0 Then
+            timeLeft = timeLeft - 1000
+            lblTime.Text = timeLeft.ToString + " seconds left."
+
+        Else
+            lblTime.Text = "Time's Up!"
+            MessageBox.Show("You didn't finish in time you idiot. He turned you into a failure pancake.")
+
+
+        End If
+    End Sub
+End Class
